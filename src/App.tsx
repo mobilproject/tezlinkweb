4f280d334e152636538cc12c511a1fd610ce20ff
Ahmad Fuzalov <36181294+digitalprojections@users.noreply.github.com>
2026-02-03 18:57:51 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,15 @@ import { LanguageProvider } from './contexts/LanguageContext';
 function App() {
   return (
     <LanguageProvider>
-      <LanguageProvider>
-        {/* Explicitly set basename for production to '/taxi/' to match deployment URL */}
-        <BrowserRouter basename={import.meta.env.PROD ? '/taxi/' : '/'}>
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/map" element={<MapPage />} />
-          </Routes>
-        </BrowserRouter>
-      </LanguageProvider>
-      );
+      {/* Explicitly set basename for production to '/taxi/' to match deployment URL */}
+      <BrowserRouter basename={import.meta.env.PROD ? '/taxi/' : '/'}>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/map" element={<MapPage />} />
+        </Routes>
+      </BrowserRouter>
+    </LanguageProvider>
+  );
 }
 
-      export default App;
+export default App;
